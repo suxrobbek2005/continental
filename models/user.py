efea256def45299a6e906b8724bf22b6f157df23
Diyorbek Jumanov <76900500+djumanov@users.noreply.github.com>
2025-01-15 11:12:29 +0500

    Update user.py

--- models/user.py
+++ models/user.py
@@ -2,5 +2,5 @@ class User:
     def __init__(self, name: str, age: int, username: str, password: str) -> None:
         self.name = name
         self.age = age
-        self.usurnaem = username
+        self.username = username
         self.password = password
